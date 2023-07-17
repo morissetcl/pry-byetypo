a3465ff8fc6d1aceeb8238b8772b36cad68b1cad
morissetcl <clement.morisset@potloc.com>
2023-07-17 18:31:33 -0400

    Initial commit

---
+++ lib/pry/cyrano.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cyrano/version"
+
+module Pry
+  module Cyrano
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pry/cyrano/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pry
+  module Cyrano
+    VERSION = "0.1.0"
+  end
+end
