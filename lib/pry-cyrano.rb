e68bfdb840ebad73149995ec67f1bebaf63c2ef9
Clément Morisset <clement.morisset@potloc.com>
2023-12-06 17:29:44 -0500

    master TMP

--- lib/pry-cyrano.rb
+++ lib/pry-cyrano.rb
@@ -2,22 +2,21 @@
 
 require 'pry'
 require 'zeitwerk'
+require 'byebug'
 
 require_relative 'pry-cyrano/version'
 
 module Pry::Cyrano
+  Pry.config.hooks.add_hook(:before_session, :eager_loading) do |output, exception, _pry_|
+    Zeitwerk::Loader.eager_load_all
+    @ar_models = ActiveRecord::Base.descendants.map { |model| model.name }
+  end
 
-  Zeitwerk::Loader.eager_load_all
-  @ar_models = ActiveRecord::Base.descendants.map { |model| model.name }
-  db_config = YAML.safe_load(ERB.new(File.read("./config/database.yml")).result, aliases: true)
+  db_config = YAML.safe_load(File.read("./config/database.yml"), aliases: true)
+  ActiveRecord::Base.establish_connection(db_config["development"])
 
-  p  db_config
-
-  ActiveRecord::Base.establish_connection("development")
-      # ActiveRecord::Base.establish_connection(db_config[env])
   singularize_table_names = ActiveRecord::Base.connection.tables.map { |a| a.chop }
   @association_dictionary = ActiveRecord::Base.connection.tables.zip(singularize_table_names).flatten
-
   @attempts = 0
   MAX_ATTEMTPS = 3
 
@@ -35,63 +34,65 @@ module Pry::Cyrano
     Pry::ExceptionHandler.handle_exception(output, exception, _pry_) unless handled_exception?(exception)
   end
 
-  def handled_exception?(exception)
-    return false if max_attempts_reached?
+  class << self
+    def handled_exception?(exception)
+      return false if max_attempts_reached?
 
-    exception.to_s.start_with?("uninitialized constant") ||
-      exception.to_s.start_with?("PG::UndefinedTable: ERROR:  missing FROM-clause entry for table") ||
-        exception.to_s.start_with?("Can't join")
-  end
+      exception.to_s.start_with?("uninitialized constant") ||
+        exception.to_s.start_with?("PG::UndefinedTable: ERROR:  missing FROM-clause entry for table") ||
+          exception.to_s.start_with?("Can't join")
+    end
 
-  def spell_checker(dictionary)
-    DidYouMean::SpellChecker.new(dictionary: dictionary)
-  end
+    def spell_checker(dictionary)
+      DidYouMean::SpellChecker.new(dictionary: dictionary)
+    end
 
-  def reset_attempts
-    @attempts = 0
-  end
+    def reset_attempts
+      @attempts = 0
+    end
 
-  def max_attempts_reached?
-    @attempts > MAX_ATTEMTPS
-  end
+    def max_attempts_reached?
+      @attempts > MAX_ATTEMTPS
+    end
 
-  def handle_uninitialized_constant(output, exception, _pry_)
-    mispelled_word = exception.to_s.split.last
-    corrected_word = spell_checker(@ar_models).correct(mispelled_word).first
+    def handle_uninitialized_constant(output, exception, _pry_)
+      mispelled_word = exception.to_s.split.last
+      corrected_word = spell_checker(@ar_models).correct(mispelled_word).first
 
-    last_cmd = Pry.line_buffer.last.strip
-    correct_cmd = last_cmd.gsub(mispelled_word, corrected_word)
+      byebug
+      last_cmd = Pry.line_buffer.last.strip
+      correct_cmd = last_cmd.gsub(mispelled_word, corrected_word)
 
-    output.puts "🤓 #{mispelled_word} does not exist, running the command with #{corrected_word} assuming is what you meant. 🤓"
-    output.puts "🤓  running #{correct_cmd} 🤓"
+      output.puts "🤓 #{mispelled_word} does not exist, running the command with #{corrected_word} assuming is what you meant. 🤓"
+      output.puts "🤓  running #{correct_cmd} 🤓"
 
-    _pry_.eval(correct_cmd)
-  end
+      _pry_.eval(correct_cmd)
+    end
 
+    def missing_from_clause_entry_table(output, exception, _pry_)
+      unknown_table_name = exception.to_s.match(/PG::UndefinedTable: ERROR:  missing FROM-clause entry for table "(.*?)"\nLINE/)[1]
+      corrected_word = spell_checker(@association_dictionary).correct(unknown_table_name).first
 
-  def missing_from_clause_entry_table(output, exception, _pry_)
-    unknown_table_name = exception.to_s.match(/PG::UndefinedTable: ERROR:  missing FROM-clause entry for table "(.*?)"\nLINE/)[1]
-    corrected_word = spell_checker(@association_dictionary).correct(unknown_table_name).first
+      last_cmd = Pry.line_buffer.last.strip
+      correct_cmd = last_cmd.gsub(/\b#{unknown_table_name}\b/, corrected_word)
 
-    last_cmd = Pry.line_buffer.last.strip
-    correct_cmd = last_cmd.gsub(/\b#{unknown_table_name}\b/, corrected_word)
+      output.puts "🤓 `#{unknown_table_name}` table relation not found, running the command with `#{corrected_word}` assuming is what you meant. 🤓"
+      output.puts "🤓  running #{correct_cmd} 🤓"
 
-    output.puts "🤓 `#{unknown_table_name}` table relation not found, running the command with `#{corrected_word}` assuming is what you meant. 🤓"
-    output.puts "🤓  running #{correct_cmd} 🤓"
+      _pry_.eval(correct_cmd)
+    end
 
-    _pry_.eval(correct_cmd)
-  end
+    def active_record_configuration_error(output, exception, _pry_)
+      unknown_association = exception.to_s.match(/association named '(.*?)'/)[1]
+      corrected_word = spell_checker(@association_dictionary).correct(unknown_association).first
 
-  def active_record_configuration_error(output, exception, _pry_)
-    unknown_association = exception.to_s.match(/association named '(.*?)'/)[1]
-    corrected_word = spell_checker(@association_dictionary).correct(unknown_association).first
+      last_cmd = Pry.line_buffer.last.strip
+      correct_cmd = last_cmd.gsub(/\b#{unknown_association}\b/, corrected_word)
 
-    last_cmd = Pry.line_buffer.last.strip
-    correct_cmd = last_cmd.gsub(/\b#{unknown_association}\b/, corrected_word)
+      output.puts "🤓 `#{unknown_association}` association not found, running the command with `#{corrected_word}` assuming is what you meant. 🤓"
+      output.puts "🤓  running #{correct_cmd} 🤓"
 
-    output.puts "🤓 `#{unknown_association}` association not found, running the command with `#{corrected_word}` assuming is what you meant. 🤓"
-    output.puts "🤓  running #{correct_cmd} 🤓"
-
-    _pry_.eval(correct_cmd)
+      _pry_.eval(correct_cmd)
+    end
   end
 end
