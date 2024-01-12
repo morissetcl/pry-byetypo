91c039a52aa0ceeae17bc9d2e34fcc2bb565c144
Clément Morisset <clement.morisset@potloc.com>
2024-01-12 16:29:22 -0500

    refactoring Refactoring checks

--- lib/pry-cyrano/setup/checks/database_pool.rb
+++ lib/pry-cyrano/setup/checks/database_pool.rb
@@ -1,29 +1,25 @@
 # frozen_string_literal: true
-require "uri"
+
+require_relative "base"
 
 module Setup
   module Checks
-    class DatabasePool
+    class DatabasePool < Base
       class << self
-        def check(database_config, logger)
-          URI.parse(database_config["pool"])
-        rescue URI::InvalidURIError
-          if ENV["DATABASE_POOL"]
-            logger.info(infer_database_pool_msg)
-            database_config["pool"] = ENV["DATABASE_POOL"]
-          else
-            logger.warn(missing_database_pool_msg)
-          end
+        def name
+          "pool"
         end
 
-        private
+        def variable
+          "DATABASE_POOL"
+        end
 
-        def missing_database_pool_msg
+        def missing_variable_msg
           "[PRY-CYRANO] ENV[\"DATABASE_POOL\"] is empty. Please add a value to it to make pry-cyrano work."
         end
 
-        def infer_database_pool_msg
-          "[PRY-CYRANO] Database pool not readable, try to connect using the ENV[\"DATABASE_POOL\"]"
+        def infer_database_variable_msg
+          "[PRY-CYRANO] Database #{name} not readable, try to connect using the ENV[\"DATABASE_POOL\"]"
         end
       end
     end
