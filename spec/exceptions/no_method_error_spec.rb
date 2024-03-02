9fcb71b2f3aa5143e0c5bf5916d3af4444b978d4
Clement Morisset <morissetcl87@gmail.com>
2024-03-02 17:22:32 -0500

    Fix array methods

--- spec/exceptions/no_method_error_spec.rb
+++ spec/exceptions/no_method_error_spec.rb
@@ -7,16 +7,30 @@ RSpec.describe Exceptions::NoMethodError do
 
   let(:output) { Pry::Output.new(pry) }
   let(:pry) { Pry.new(output: StringIO.new) }
-  let(:exception) { NoMethodError.new("undefined method `ancestrs' for User:Class") }
-  let(:last_cmd) { "User.ancestrs" }
-  let(:corrected_cmd) { "User.ancestors" }
 
   describe "#call" do
     before { allow(Pry).to receive(:line_buffer).and_return([last_cmd]) }
 
-    it "outputs a corrected command and runs it" do
-      expect(pry).to receive(:eval).with(corrected_cmd)
-      subject
+    context "given an undefined method for a model" do
+      let(:last_cmd) { "User.ancestrs" }
+      let(:exception) { NoMethodError.new("undefined method `ancestrs' for User:Class") }
+      let(:corrected_cmd) { "User.ancestors" }
+
+      it "outputs a corrected command and runs it" do
+        expect(pry).to receive(:eval).with(corrected_cmd)
+        subject
+      end
+    end
+
+    context "given an undefined method for a built in class" do
+      let(:last_cmd) { "[1].firsst" }
+      let(:exception) { NoMethodError.new("undefined method `firsst' for an instance of Array") }
+      let(:corrected_cmd) { "[1].first" }
+
+      it "outputs a corrected command and runs it" do
+        expect(pry).to receive(:eval).with(corrected_cmd)
+        subject
+      end
     end
   end
 end
